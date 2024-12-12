50bd3d7e0c96383603a8639a7746cb1e889c6884
Azmi Nabeel <70376685+azmi-nabeel@users.noreply.github.com>
2024-12-12 17:07:22 +0530

    Update page.tsx

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center bg-gradient-defang">
-      <h1 className="text-4xl font-bold text-white">Next.js &times; Defang</h1>
+      <h1 className="text-4xl font-bold text-white">Defang Challenge 1: 1-Click Deploy</h1>
+      <h2 className="text-4xl font-bold text-white">Next.js &times; Defang</h2>
     </main>
   );
 }
